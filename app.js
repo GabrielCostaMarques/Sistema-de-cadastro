2d1dbe44065786694ec6f1f6c50e16ed1ad8e5c5
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-01-04 10:53:40 -0300

    Adicionando um log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("Alterando alguma coisa");
 console.log("O paulo fez esse commit sem vc ver na aula");
+console.log("Enviando para o github");
